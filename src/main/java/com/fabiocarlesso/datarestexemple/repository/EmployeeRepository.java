19ca31d8665e4690dde70b007ace83454881225f
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-11-10 16:13:50 -0300

    feat(DataRestExemple): Restricting Delete HTTP Method

--- src/main/java/com/fabiocarlesso/datarestexemple/repository/EmployeeRepository.java
+++ src/main/java/com/fabiocarlesso/datarestexemple/repository/EmployeeRepository.java
@@ -5,10 +5,15 @@ import com.fabiocarlesso.datarestexemple.domain.view.EmployeeView;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 @RepositoryRestResource(path="members", excerptProjection = EmployeeView.class)
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
     List<Employee> findByFirstName(@Param("firstName") String firstName);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
 }
